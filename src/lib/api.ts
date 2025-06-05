c8ba268e15567bd531edc6797d1e0c2bc4aa7b0c
João Goulart <joaorgoulartt@gmail.com>
2025-06-04 21:43:34 -0300

    testing

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,7 +7,7 @@ import {
 } from "@/types";
 
 const API_BASE_URL =
-    process.env.NEXT_PUBLIC_API_URL;
+    process.env.NEXT_PUBLIC_API_URL || "https://fides-fides-api.kz9t7s.easypanel.host";
 
 interface ApiResponse<T = any> {
     success: boolean;
